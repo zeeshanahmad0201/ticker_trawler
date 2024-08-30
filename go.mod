4268a8b285d3ddee125e533e8f547da8dc3b43d0
Zeeshan Ahmad <xeeshanahmad0201@gmail.com>
2024-08-30 11:02:54 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zeeshanahmad0201/ticker_trawler.git
+
+go 1.21.13
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Ticker Trawler - A simple stock ticker price and ratios fetcher")
+
+}
